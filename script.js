e147521457bead16f4b4438eecb255f3c68c815b
Caio Lucas <clucaszrtmain@gmail.com>
2025-11-06 22:37:18 -0300

    feat: Validação para inserir um ano válido

---
+++ script.js
@@ -0,0 +1,14 @@
+let botaoSubmit = document.querySelector(".btn-submit")
+let formularioAno = document.querySelector("#txtano")
+let telaResultado = document.querySelector("#res")
+let botoesSexo = document.getElementsByClassName("radsex")
+
+botaoSubmit.addEventListener("click", verificarIdade)
+
+function verificarIdade(){
+    let dataAtual = new Date()
+    let anoAtual = dataAtual.getFullYear
+    if(formularioAno.value == 0 || formularioAno.value > anoAtual){
+        alert("Insira um ano Válido")
+    }
+}
